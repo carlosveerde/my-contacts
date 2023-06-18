f7582f04513f2d0a96490034ec69cd457f54a5af
Verde <131483292+carlosveerde@users.noreply.github.com>
2023-06-18 15:44:34 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    const img = document.querySelector("#profile img")
+
+ if(html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light-cropped.png')
+    img.setAttribute('alt', 'Carlos de braços cruzados')
+
+ } else {
+    img.setAttribute('src', './assets/avatar-dark-cropped.png')
+    img.setAttribute('alt', 'Carlos de braços cruzados com óculos escuros')
+ }
+}
